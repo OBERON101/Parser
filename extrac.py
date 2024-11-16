ea4faaba7769365081a0f6e7dc4eb5f7552388f6
erwrej <h-rustam2011xp@mail.ru>
2024-11-16 12:26:41 +0300

    captcha fix + price extraction

--- extrac.py
+++ extrac.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 
-import re
 import logging
+
 
 def delete_double_spaces(string):
     while '  ' in string:
         string = string.replace('  ', ' ')
     return string
 
+
 def get_attribute_values(tree):
     try:
-        return list(map(lambda span: delete_double_spaces(span.text.replace('\n', '').strip().replace('  ', ' ')), tree.xpath('//span[contains(@class, "YwVL7 _2SUA6 _3kbFf IFARr _1A5yJ")]/child::span')))
+        return list(map(lambda span: delete_double_spaces(span.text.replace('\n', '').strip().replace('  ', ' ')),
+                        tree.xpath('//span[contains(@class, "YwVL7 _2SUA6 _3kbFf IFARr _1A5yJ")]/child::span')))
     except IndexError:
-        logging.warning()
+        logging.warning('Не удалось извлечь значения аттрибутов продукта')
         return None
+
 
 def get_attribute_names(tree):
     try:
-        return int(filter(lambda h3: '&thinsp;' in h3.text, tree.xpath('//h3[contains(@data-auto, "snippet-price-current")]'))[0].text.replace('&thinsp;', ''))
+        return list(map(lambda span: delete_double_spaces(span.text.replace('\n', '').strip().replace('  ', ' ')),
+                        tree.xpath('//span[contains(@class, "_1EbOn _2SUA6 _3kbFf IFARr _1A5yJ")]')))
     except IndexError:
-        logging.warning()
+        logging.warning('Не удалось извлечь наименования аттрибутов продукта')
         return None
 
 
@@ -30,7 +34,26 @@ def get_attributes_of_product(tree):
         attributes = {}
         for i in range(len(attribute_names)):
             attributes[attribute_names[i]] = attribute_values[i]
+        attributes['price'] = get_price(tree)
+        attributes['price_without_yandex_card'], attributes['price_without_discount'] = get_price_alternative(tree)
         return attributes
     except IndexError:
-        logging.warning()
+        logging.warning('Не удалось извлечь аттрибуты продукта')
         return None
+
+
+def get_price(tree):
+    try:
+        return int(list(filter(lambda h3: 'Цена с картой' in h3.text_content(),
+                               tree.xpath('//h3[contains(@data-auto, "snippet-price-current")]')))[0]
+                   .text_content().replace('Цена с картой Яндекс Пэй:', '').replace(' ', '').replace(' ₽', ''))
+    except IndexError:
+        raise IndexError('Не удалось извлечь цену продукта')
+
+
+def get_price_alternative(tree):
+    try:
+        return list(map(lambda x: int(x), filter(lambda x: x != '', map(lambda x: x.replace('\u2009', ''), tree.xpath(
+            '//span[contains(@data-auto, "snippet-price-old")]')[0].text_content().replace('без:\xa0Вместо: ','').split('\u2009₽')))))
+    except IndexError:
+        raise IndexError('Не удалось извлечь цену продукта')
--- scrape_xpaths.py
+++ scrape_xpaths.py
@@ -1,14 +1,19 @@
-from random import randint
-import pandas
-from lxml import html
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import requests
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
-import time
-import os
 import logging
+import os
+import time
 
+from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+
+def try_captcha(web_driver):
+    try:
+        button = web_driver.find_element(By.XPATH, "//input[contains(@class, 'CheckboxCaptcha-Button')]")
+        button.click()
+        time.sleep(3)
+    except Exception as e:
+        logging.warning("Не удалось найти кнопку капчи")
+        print("Не удалось найти кнопку капчи")
 
 # Настройка логирования
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,9 +21,17 @@ logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s 
 begin_sleep_time = 5
 html_pages_count = 1000
 time_between_clicks = 2
-next_card_sleep_time = 15
+next_card_sleep_time = 2
+
+skipLinkLoading = int(input('Пропустить загрузку ссылок? Да - 1, нет - 0: '))
 
 driver = webdriver.Firefox()
+
+s = 'w'
+if skipLinkLoading:
+    s = 'r'
+    html_pages_count = 100
+
 driver.get("https://market.yandex.ru/catalog--noutbuki/26895412/list?hid=91013&local-offers-first=0")
 time.sleep(begin_sleep_time)
 title_name = "Ноутбуки"
@@ -37,7 +50,8 @@ links_count = 0
 last_links_count = 0
 max_not_scrolled_count = 5
 not_scrolled_count = 0
-with open('links.txt', 'w') as f:
+
+with open('links.txt', s) as f:
     collected_links = set()
     while links_count < html_pages_count:
         links = driver.find_elements(By.XPATH, "//a[contains(@class, 'EQlfk Gqfzd')]")
@@ -46,8 +60,9 @@ with open('links.txt', 'w') as f:
             if href not in collected_links:
                 collected_links.add(href)
                 links_count += 1
-                print("Добавлена ссылка: " + href)
-                f.write(href + "\n")
+                if not skipLinkLoading:
+                    print("Добавлена ссылка: " + href)
+                    f.write(href + "\n")
 
         if last_links_count == links_count:
             if not_scrolled_count == max_not_scrolled_count:
@@ -75,7 +90,8 @@ with open('links.txt', 'r') as f:
             all_specs_button = driver.find_element(By.XPATH, "//span[contains(@class,'_1_47u _2SUA6 _33utW IFARr _1A5yJ')]")
             all_specs_button.click()
             time.sleep(time_between_clicks)
-            all_specs_button.click()
+            if driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML") is None:
+                all_specs_button.click()
             time.sleep(next_card_sleep_time)
             # Получаем HTML-код страницы товара через Script
             response = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
@@ -87,6 +103,10 @@ with open('links.txt', 'r') as f:
         except ElementClickInterceptedException:
             logging.error(f"Ошибка при загрузке страницы: {link}")
             print(f"Ошибка при загрузке страницы: {link}")
+        except Exception as e:
+            logging.warning("Попытка пройти капчу")
+            print("Попытка пройти капчу")
+            try_captcha(driver)
 
 print("Загрузка завершена.")
 logging.info("Загрузка завершена.")
